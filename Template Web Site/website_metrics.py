15825a721fa4ea5401629299b529546792c65e7d
Alexander Shelyakin <a.shelyakin@itmicus.ru>
2019-11-04 01:30:34 +0300

    website metrics: migrate data from zabbix-sender to depended items

--- Template Web Site/website_metrics.py
+++ Template Web Site/website_metrics.py
@@ -25,7 +25,7 @@ import website_settings
 __author__ = "Pavel Kuznetsov - https://itmicus.ru"
 __copyright__ = "Copyright 2018, Itmicus LLC, Pavel Kuznetsov"
 __license__ = "Mozilla Public License"
-__version__ = "0.9"
+__version__ = "1.0"
 __email__ = "p.kuznetsov@itmicus.ru"
 __status__ = "Production ready"
 __doc__ = """This script is part of Template_Website_metrics.xml Zabbix Monitoring template.
@@ -42,7 +42,6 @@ __doc__ = """This script is part of Template_Website_metrics.xml Zabbix Monitori
             1. pyOpenSSL brokes sock.getpeercert() and --testssl not work
 
             """
-
 
 # this code need to add cert properties for SSL connection inside response
 HTTPResponse = requests.packages.urllib3.response.HTTPResponse #python2/3
@@ -72,39 +71,6 @@ def new_HTTPAdapter_build_response(self, request, resp):
 
 
 HTTPAdapter.build_response = new_HTTPAdapter_build_response
-
-
-def zbx_sender(zbx_hostname, data):
-    """
-    Sending data to Zabbix server via zabbix_sender utility.
-    :param zbx_hostname: HostName as zabbix server configuration
-    :param data: array of [[zabbix item, value], [zabbix item, value]]
-    :return:
-       return output from zabbix_sender command
-    """
-    cmd = [website_settings.zabbix_sender_config['zbx_sender'], '-z', website_settings.zabbix_sender_config['zbx_server'],
-           '-p', website_settings.zabbix_sender_config['zbx_server_port'], '-r', '-i', '-']
-
-    if logging.getLevelName(logging.getLogger().getEffectiveLevel()) == 'DEBUG':
-        cmd.insert(1, '-vv')
-
-    fmt = "{host} {key} {value}"
-    values_list = [fmt.format(host=zbx_hostname, key=v[0], value=v[1])
-                   for v in data]
-    values = "\n".join(values_list)
-
-    logging.debug('Zabbix sender:' + str(cmd))
-    logging.debug('Items: ' + str(values))
-
-    p = subprocess.Popen(cmd, stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result = p.communicate(input=values.encode('utf8'))
-    out = result[0]+result[1]
-
-    logging.debug('Zabbix sender result: ' + str(out))
-
-    return out.decode('utf8')
-
 
 class WebSiteCheck:
     """
@@ -208,7 +174,6 @@ class WebSiteCheck:
         domain_info = json.loads(domain_info.text)
         logging.debug('Domain check result ' + str(domain_info))
 
-        notBefore = parser.parse(domain_info['created_on'])
         expire_date = parser.parse(domain_info['expires_on'])
         expire_in = expire_date.replace(tzinfo=None) - datetime.utcnow().replace(tzinfo=None)
         days_to_expire = 0
@@ -224,8 +189,8 @@ class WebSiteCheck:
         data = {
             'domain_name': domain_info['domain'],
             'registrar': registrar,
-            'creation_date': str(notBefore),
-            'expiration_date': str(expire_date),
+            'creation_date': datetime.strptime(domain_info['created_on'], '%Y-%m-%dT%H:%M:%S.000Z'),
+            'expiration_date': datetime.strptime(domain_info['expires_on'], '%Y-%m-%dT%H:%M:%S.000Z'),
             'days_to_expire': days_to_expire,
         }
         logging.debug('Return data:'+str(data))
@@ -236,6 +201,8 @@ class WebSiteCheck:
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36"
         headers = {'User-Agent': user_agent}
         phrase_status = 0
+        if timeout_value > 20:
+            timeout_value = 20
         try:
             response = self.session.get(
                 url, headers=headers, timeout=(timeout_value, timeout_value))
@@ -247,7 +214,6 @@ class WebSiteCheck:
             else:
                 text = response.text.encode('utf8') # encode, if python2
 
-            #logging.debug('Text:'+str(text)) #too many noise, disable
             length = len(response.content)
             speed = round(length/time)
             status = 1
@@ -276,7 +242,6 @@ class WebSiteCheck:
         }
         return data
 
-
 def output_json_lld(lld_name, objects):
     array = []
     items = {}
@@ -286,19 +251,8 @@ def output_json_lld(lld_name, objects):
     result = json.dumps({'data': array}, indent=4, separators=(',', ':'))
     return result
 
-
-def send_data(hostname, data):
-    logging.debug('Send data: ' + str(data))
-    result = ''
-    if hostname is not None and hostname != '':
-        out = zbx_sender(hostname, data)
-        result = {'zbx_sender': out}
-
-    return result
-
-
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Website metrics script helps to parse, checks SSL certs and website.')
 
     parser.add_argument(
         '--discoverydomain', help='Discovery domain name from URL', type=str)
@@ -306,9 +260,6 @@ def main():
     parser.add_argument(
         '--discoveryssl', help='Discovery SSL from URL', type=str)
     
-    parser.add_argument(
-        '--hostname', help='Zabbix item hostname', type=str)
-
     parser.add_argument(
         '--testssl', help='URL for check SSL certificate', type=str)
 
@@ -322,7 +273,7 @@ def main():
         '--testphrase', help='Test web site with phrase', type=str)
 
     parser.add_argument(
-        '--timeout', help='Timeout for web site test', type=str, default='15')
+        '--timeout', help='Timeout for web site test, sec', type=str, default='15')
 
     parser.add_argument('--httpproxy',
                         help='Specify a proxy to check SSL. For example: username:password@serverfqdn:port',
@@ -333,6 +284,8 @@ def main():
 
     parser.add_argument('--debug', help='Debug mode with verbose output',
                         type=str, default='False')
+    
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
     args = parser.parse_args()
 
@@ -342,7 +295,6 @@ def main():
     test_ssl = args.testssl
     test_site = args.testsite
     test_phrase = args.testphrase
-    zbx_hostname = args.hostname
     http_proxy = args.httpproxy
     timeout = args.timeout
 
@@ -362,6 +314,7 @@ def main():
 
     web_checks = WebSiteCheck(http_proxy, use_proxy)
 
+    # Discovery block
     # get registred domain from URL
     if discoverydomain is not None:
         try:
@@ -385,66 +338,63 @@ def main():
             return 1
         except:
             return 0
+    # Discovery block end
 
+    # return JSON for Domain items prototype
     if test_domain is not None and test_domain != '':
         try:
-            data = []
+            data = dict()
             domainname_result = web_checks.domain_get_status(test_domain)
             if domainname_result['days_to_expire'] > 0:
-                data.append(['website_metrics.domain.status[{0}]'.format(test_domain), 1])
-                data.append(['website_metrics.domain.daystoexpire[{0}]'.format(test_domain), str(
-                    domainname_result['days_to_expire'])])
-                data.append(['website_metrics.domain.registrar[{0}]'.format(test_domain), str(
-                    domainname_result['registrar'])])
-                data.append(['website_metrics.domain.creationdate[{0}]'.format(test_domain), str(
-                    domainname_result['creation_date'])])
-                data.append(['website_metrics.domain.expiredate[{0}]'.format(test_domain), str(
-                    domainname_result['expiration_date'])])
+                data.update({'domain_status': 1})
+                data.update({'domain_daystoexpire': domainname_result['days_to_expire']})
+                data.update({'domain_registrar': domainname_result['registrar']})
+                data.update({'domain_creationdate': domainname_result['creation_date'].strftime("%c")})
+                data.update({'domain_expiredate': domainname_result['expiration_date'].strftime("%c")})
             else:
-                data.append(['website_metrics.domain.status[{0}]'.format(test_domain), 0])
-            send_data(zbx_hostname, data)
+                data.update({'domain_status': 0})
+            json.dump(data, sys.stdout)
             return 1
         except:
             return 0
 
+    # return JSON for SSL items prototype
     if test_ssl is not None and test_ssl != '':
         try:
             cert_is_valid = web_checks.ssl_verify_cert(test_ssl)
-            data = []
-            data.append(['website_metrics.ssl.status[{0}]'.format(test_ssl), str(cert_is_valid)])
+            data = {
+                'ssl_status' : cert_is_valid
+            }
             if cert_is_valid:
                 ssl_data = web_checks.ssl_get_status(test_ssl)
-                data.append(['website_metrics.ssl.daystoexpire[{0}]'.format(test_ssl),
-                            str(ssl_data['days_to_expire'])])
-                data.append(['website_metrics.ssl.issuedby[{0}]'.format(test_ssl),
-                            str(ssl_data['issued_by'])])
-                data.append(['website_metrics.ssl.issueddate[{0}]'.format(test_ssl),
-                            str(ssl_data['issued_date'])])
-                data.append(['website_metrics.ssl.expiredate[{0}]'.format(test_ssl),
-                            str(ssl_data['expiry_date'])])
-                data.append(['website_metrics.ssl.serialnumber[{0}]'.format(test_ssl),
-                            str(ssl_data['serial_number'])])
-            send_data(zbx_hostname, data)
+                data.update({'ssl_daystoexpire': ssl_data['days_to_expire']})
+                data.update({'ssl_issuedby': ssl_data['issued_by']})
+                data.update({'ssl_expiredate': ssl_data['expiry_date'].strftime("%c")})
+                data.update({'ssl_issueddate': ssl_data['issued_date'].strftime("%c")})
+                data.update({'ssl_serialnumber': ssl_data['serial_number']})
+            json.dump(data, sys.stdout)
             return 1
         except:
             return 0
 
     if test_site is not None and test_site != '':
         try:
-            webtest = web_checks.website_get_status(
-                test_site, test_phrase, int(timeout))
-            data = []
-            data.append(['website_metrics.test.status', str(webtest['status'])])
-            data.append(['website_metrics.test.time', str(webtest['time'])])
-            data.append(['website_metrics.test.code', str(webtest['code'])])
-            data.append(['website_metrics.test.speed', str(webtest['speed'])])
-            data.append(['website_metrics.test.message', str(webtest['message'])])
-            data.append(['website_metrics.test.phrase_status',
-                        str(webtest['phrase_status'])])
-            send_data(zbx_hostname, data)
+            webtest = web_checks.website_get_status(test_site, test_phrase, timeout)
+            data = {'test_status': webtest['status']}
+            data.update({'test_time': webtest['time']})
+            data.update({'test_code': webtest['code']})
+            data.update({'test_speed': webtest['speed']})
+            data.update({'test_message': webtest['message']})
+            data.update({'test_phrase_status': webtest['phrase_status']})
+            json.dump(data, sys.stdout)
             return 1
         except:
             return 0
+    
+    # print help message if no args passed
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
